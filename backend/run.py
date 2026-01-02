af4815a1a63d03355692e5cf68addb1ae8d6f6f4
Kimviruscool <sinsa9122@naver.com>
2026-01-02 19:52:12 +0900

    firebase

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,7 @@
 from app import create_app
 
+# create_app 함수를 통해 Flask 앱 객체 생성
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
